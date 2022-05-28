52596c0c09750c4fd3c2e1d025e1108ea5870a16
9ice_guy <clintonufere@gmail.com>
2022-05-28 06:01:59 +0100

    Changed firebase sdk

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,25 +5,8 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBUPD7ITA7Bl2WUXbIO7jaaT_ahuOGfP2E",
-//   authDomain: "project-manager-b6a9c.firebaseapp.com",
-//   projectId: "project-manager-b6a9c",
-//   storageBucket: "project-manager-b6a9c.appspot.com",
-//   messagingSenderId: "288216885564",
-//   appId: "1:288216885564:web:74d722e9b27341885b5639",
-//   measurementId: "G-Z1PEBYZL67",
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBUPD7ITA7Bl2WUXbIO7jaaT_ahuOGfP2E",
-  authDomain: "project-manager-b6a9c.firebaseapp.com",
-  projectId: "project-manager-b6a9c",
-  storageBucket: "project-manager-b6a9c.appspot.com",
-  messagingSenderId: "288216885564",
-  appId: "1:288216885564:web:3c0eb5d71b9893cb5b5639",
-  measurementId: "G-YMCEPQMLE4",
+ 
 };
 
 // Initialize Firebase
